b5726e6e108fcfc43337c614159300ab515ec83b
VickyVariety <vickyfyrehart@gmail.com>
2023-12-29 22:21:39 +0100

    Fixed the slideshow gallery.. and then messed it up a little bit. Oh well.

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,35 +1,42 @@
-var slideIndex = 1; /* Startindex för bildspelet */
-showSlides(slideIndex); /* Visa den första bilden */
+var slideIndex = 1; // Startindex för bildspelet, börjar med den första bilden
+showSlides(slideIndex); // Visar den första bilden vid inläsning
 
-// Funktioner för att kontrollera nästa/föregående bild
+// Funktion för att gå framåt eller bakåt i bildspelet
 function plusSlides(n) {
-  showSlides(slideIndex += n); /* Ändrar den aktuella bilden baserat på input */
+  showSlides(slideIndex += n); // Uppdaterar slideIndex och visar nästa/föregående bild
 }
 
-// Funktion för att styra vilken bild som är aktiv baserat på miniatyrbildernas kontroller
+// Funktion för att visa en specifik bild när användaren klickar på en miniatyrbild
 function currentSlide(n) {
-  showSlides(slideIndex = n); /* Visar den valda bilden */
-  updateThumbnails(n);
+  showSlides(slideIndex = n); // Sätter slideIndex till den valda bilden och visar den
+  updateThumbnails(n); // Uppdaterar miniatyrbilderna för att markera den aktiva bilden
 }
 
+// Funktion för att visa och dölja bilder baserat på slideIndex
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides"); /* Samling av alla bilder */
-  var dots = document.getElementsByClassName("dot"); /* Samling av alla punkter */
-  if (n > slides.length) {slideIndex = 1} /* Återställ till första bilden om n överstiger antalet bilder */
-  if (n < 1) {slideIndex = slides.length} /* Gå till sista bilden om n är mindre än 1 */
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  /* Göm alla bilder */
+  var slides = document.getElementsByClassName("mySlides"); // Hämtar alla bilder
+  var dots = document.getElementsByClassName("dot"); // Hämtar alla punkter
+  // Loopar igenom alla bilder och döljer dem
+  for (var i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", ""); /* Ta bort 'active' klassen från alla punkter */
+  // Loopar igenom alla punkter och tar bort 'active' klassen
+  for (var i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  // Visar den aktuella bilden och markerar motsvarande punkt
+  slides[slideIndex-1].style.display = "block"; // Visar den aktuella bilden
+  dots[slideIndex-1].className += " active"; // Lägger till 'active' klassen på motsvarande punkt
 }
 
-function updateThumbnails(n) {
-  var thumbnails = document.getElementsByName("demo");
-  for (var i = 0; i <thumbnails.length; i++)) {
-    thumbnails[i].style.opacity = (i + 1 === n) ? "1" : "0.6"; // Markerar den aktiva miniatyrbilden
+// Funktion för att uppdatera miniatyrbilderna baserat på vilken bild som är aktiv
+function updateThumbnails(activeIndex) {
+  var thumbnails = document.getElementsByClassName("demo"); // Hämtar alla miniatyrbilder
+  // Loopar igenom miniatyrbilderna och uppdaterar opacitet och 'active' klassen
+  for (var i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].className = thumbnails[i].className.replace(" active", "");
+    thumbnails[i].style.opacity = "0.6"; // Sätter standard opacitet för miniatyrbilder
   }
+  thumbnails[activeIndex - 1].className += " active"; // Lägger till 'active' klassen på den aktiva miniatyrbilden
+  thumbnails[activeIndex - 1].style.opacity = "1"; // Sätter full opacitet på den aktiva miniatyrbilden
 }
