d136076a3b19cabd1cb5caec4aabf14a1a05f5a2
VickyVariety <vickyfyrehart@gmail.com>
2023-12-30 14:05:46 +0100

    Added currentSlide-function and updated alt-texts

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -13,12 +13,25 @@ function showSlides(n) {
   for (var i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+
+  // Uppdatera opaciteten för alla thumbnails
+  var thumbnails = document.getElementsByClassName("demo");
+  for (var i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].style.opacity = "0.6"; // Återställer opaciteten
+    thumbnails[i].className = thumbnails[i].className.replace(" active", "")
+  }
+
   // Hämtar den aktuella bilden
   var currentSlide = slides[n - 1];
   // Visar den aktuella bilden
   currentSlide.style.display = "block";
   // Markerar motsvarande punkt
   dots[n - 1].className += " active";
+
+  // Uppdaterar thumbnail för den aktuella bilden
+  var currentThumbnail = thumbnails[n - 1];
+  currentThumbnail.style.opacity = "1"; // Ökar opaciteten för den aktiva thumbnail-bilden
+  currentThumbnail.className += " active";
 }
 
 // Funktion för att klicka på nästa bild
@@ -33,6 +46,11 @@ function minusSlides(n) {
   slideIndex -= n;
   if (slideIndex < 1) { slideIndex = slides.length; }  // Går till sista bilden
   showSlides(slideIndex);
+}
+
+// Funktion för att visa en specifik bild när man klickar på en thumbnail
+function currentSlide(n){
+  showSlides(slideIndex = n);
 }
 
 // Visar den första bilden
